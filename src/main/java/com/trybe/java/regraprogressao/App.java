2b657e95c9e3e1ac1ed85a5d1e8e25bddc98cd4b
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-11-01 15:05:46 -0300

    Requisito 04

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -21,6 +21,7 @@ public class App {
     int[] grades = new int[quantityActivities];
 
     int sumWeights = 0;
+    int sumGrades = 0;
 
     for (int i = 0; i < quantityActivities; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
@@ -32,10 +33,22 @@ public class App {
 
       System.out.println("Digite a nota obtida para " + names[i] + ":");
       grades[i] = Integer.parseInt(input.nextLine());
+      sumGrades += weights[i] * grades[i];
     }
 
     if (sumWeights != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    }
+
+    double approvalPercentage = (double) sumGrades / sumWeights;
+
+    if (approvalPercentage >= 85.0) {
+      System.out.println("Parabéns! Você alcançou " + approvalPercentage
+          + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação"
+          + "alcançada neste período, " + approvalPercentage
+          + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
 
     input.close();
